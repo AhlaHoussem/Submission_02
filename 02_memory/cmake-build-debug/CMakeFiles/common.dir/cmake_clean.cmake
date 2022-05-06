3a4e426acab62104e66db0e92afc382918462401
Be grateful <jaba.houssem@gmail.com>
2022-05-06 15:36:23 +0200

    pract

---
+++ 02_memory/cmake-build-debug/CMakeFiles/common.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/common.dir/src/food.cpp.obj"
+  "CMakeFiles/common.dir/src/map.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/common.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 02_memory/cmake-build-debug/CMakeFiles/tests.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Be grateful/Desktop/FUN/3/C++ Softwarepraktikum/Neu/2/02_memory/src/food.test.cpp" "C:/Users/Be grateful/Desktop/FUN/3/C++ Softwarepraktikum/Neu/2/02_memory/cmake-build-debug/CMakeFiles/tests.dir/src/food.test.cpp.obj"
+  "C:/Users/Be grateful/Desktop/FUN/3/C++ Softwarepraktikum/Neu/2/02_memory/src/map.test.cpp" "C:/Users/Be grateful/Desktop/FUN/3/C++ Softwarepraktikum/Neu/2/02_memory/cmake-build-debug/CMakeFiles/tests.dir/src/map.test.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "_deps/catch2-src/src/catch2/.."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/Users/Be grateful/Desktop/FUN/3/C++ Softwarepraktikum/Neu/2/02_memory/cmake-build-debug/_deps/catch2-build/src/CMakeFiles/Catch2WithMain.dir/DependInfo.cmake"
+  "C:/Users/Be grateful/Desktop/FUN/3/C++ Softwarepraktikum/Neu/2/02_memory/cmake-build-debug/_deps/catch2-build/src/CMakeFiles/Catch2.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
